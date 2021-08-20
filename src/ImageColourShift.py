cbc3f847cee3ea35657152294d803224f7c9e32e
Piotr <piotrlis555@gmail.com>
2021-08-20 03:15:16 +0200

    started to put everything together

--- src/ImageColourShift.py
+++ src/ImageColourShift.py
@@ -3,7 +3,14 @@ from DirectoryTraversal import queue
 
 
 def userinput():
-    
+
+
+def help_prompt():
+    print("shift [-p] [-wt] [-bt] [[-i] or [-g]]\n\n" +
+          "     -p          determines the tolerance of colours will be considered as background\n" +
+          "     -wt         determines how close to white a colour must be in order to be considered white\n" +
+          "     -bt         determiens how close to black a colour must be in order to be considered black\n")
+
 
 def core():
     print(sys.argv)
